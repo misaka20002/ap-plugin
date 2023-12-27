6031356e89bb08e646f6e0f0e190dd8e647ae9f8
misaka20002 <misakamisaka20002@gmail.com>
2023-12-27 14:32:05 +0800

    当不允许绘多图时，禁止重复发起绘图

--- apps/aiPainting.js
+++ apps/aiPainting.js
@@ -88,6 +88,13 @@ export class Ai_Painting extends plugin {
     // 判断是否允许绘制多图
     if (paramdata.num > 1 && !current_group_policy.allowed_paint_more) return await e.reply("只可以绘制1张图哦，有需要请找管理员", false, { recallMsg: 15 })
 
+    
+    // 当不允许绘多图时，禁止重复发起绘图
+    if (!current_group_policy.allowed_paint_more && remaining_tasks) {
+      CD.clearCD(e);
+      return await e.reply(`当前已有批量绘图任务进行中，剩余${remaining_tasks}张图，请稍候`, true);
+    }
+
 
     // 禁止重复发起批量绘图
     if (paramdata.num > 1 && remaining_tasks) {
